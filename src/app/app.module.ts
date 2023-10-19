f0a008df63c247137243e6bfb66edec3dccba6f9
Pravalikapi <121709052+Pravalikapi@users.noreply.github.com>
2023-10-19 14:38:48 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyActivitiesComponent } from './components/my-activities/my-activities.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyActivitiesComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
